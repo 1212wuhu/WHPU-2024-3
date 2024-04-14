29e473cc180be1586d790050f2f79d61ffd64591
JACKYMYPERSON <108354972+JACKYMYPERSON@users.noreply.github.com>
2024-04-14 21:14:22 +0800

    Update downmark

--- Welcome/resources/views/stu/new/yourClass.blade.php
+++ Welcome/resources/views/stu/new/yourClass.blade.php
@@ -329,7 +329,7 @@
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
                 <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; Harbin Institute of Technology , Weihai</span>
+                    <span>Copyright &copy; Wuhan Polytechnic University , Jinyinhu</span>
                 </div>
             </div>
         </footer>
