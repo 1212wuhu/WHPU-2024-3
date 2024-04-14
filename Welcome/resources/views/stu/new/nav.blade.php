b2a26bfc7bea11072ef77f87814b1de90ee517ac
JACKYMYPERSON <108354972+JACKYMYPERSON@users.noreply.github.com>
2024-04-14 21:02:11 +0800

    Update 智能问答

--- Welcome/resources/views/stu/new/nav.blade.php
+++ Welcome/resources/views/stu/new/nav.blade.php
@@ -10,7 +10,7 @@
     <meta name="author" content="">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>迎新系统-哈尔滨工业大学（威海）</title>
+    <title>武汉轻工大学</title>
 
     <!-- Custom fonts for this template-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.7.2/css/all.min.css"
@@ -48,7 +48,7 @@
             <div class="sidebar-brand-icon rotate-n-15">
                 <i class="fas fa-laugh-wink"></i>
             </div>
-            <div class="sidebar-brand-text mx-3">迎新系统 <sup id="user-type">{{$sysType}}</sup></div>
+            <div class="sidebar-brand-text mx-3">智能问答系统</div>
         </a>
 
         <!-- Divider -->
