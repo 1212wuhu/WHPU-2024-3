9ae82aa3b4a5436c3dc5c4eeb519a20ffa6624e1
JACKYMYPERSON <108354972+JACKYMYPERSON@users.noreply.github.com>
2024-04-14 20:44:53 +0800

    Update countryblade

--- Welcome/resources/views/stu/new/yourCountryFolk.blade.php
+++ Welcome/resources/views/stu/new/yourCountryFolk.blade.php
@@ -53,7 +53,7 @@
 
         <!-- Heading -->
         <div class="sidebar-heading">
-            信息查询
+            核心系统
         </div>
 
         <!-- Nav Item - Information Query -->
@@ -77,7 +77,7 @@
         <li class="nav-item">
             <a class="nav-link" href="{{url('/stu/nav')}}">
                 <i class="fas fa-fw fa-plane-arrival"></i>
-                <span>到站信息</span>
+                <span>教育智能推荐</span>
             </a>
         </li>
 
@@ -96,51 +96,7 @@
                 <span>所有通知</span></a>
         </li>
 
-        <!-- Divider -->
-        <hr class="sidebar-divider">
-
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            迎新服务
-        </div>
-
-        <!-- Nav Item - welcome -->
-        <li class="nav-item">
-            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseWel"
-               aria-expanded="true" aria-controls="collapseWel">
-                <i class="fas fa-fw fa-route"></i>
-                <span>报到流程</span>
-            </a>
-            <div id="collapseWel" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                <div class="bg-white py-2 collapse-inner rounded">
-                    <h6 class="collapse-header">你可以查看：</h6>
-                    <a class="collapse-item" href="{{url('/stu/enrollInfo')}}">报到说明</a>
-                    <a class="collapse-item" href="{{url('/stu/enrollGuide')}}">开始报到</a>
-                </div>
-            </div>
-        </li>
-
-        <!-- Divider -->
-        <hr class="sidebar-divider d-none d-md-block">
-
-        <!-- Heading -->
-        <div class="sidebar-heading">
-            信息填报
-        </div>
-
-        <!-- Nav Item - selfInfo -->
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('/stu/personalInfo')}}">
-                <i class="fas fa-fw fa-info"></i>
-                <span>个人信息</span></a>
-        </li>
-
-        <!-- Nav Item - GreenPath -->
-        <li class="nav-item">
-            <a class="nav-link" href="{{url('/stu/greenPath')}}">
-                <i class="fas fa-fw fa-hands-helping"></i>
-                <span>绿色通道</span></a>
-        </li>
+        
 
         <!-- Divider -->
         <hr class="sidebar-divider d-none d-md-block">
